cf76817a5a28f5c364ebff81c1b0da71b7910275
Sergio <sergio-guitar@yandex.ru>
2023-04-13 12:35:02 +0300

    "10 task completed"

---
+++ HW_2/TASK10.py
@@ -0,0 +1,12 @@
+import random
+counter = 0
+N = int(input("Введите количество монеток"))
+for i in range(N):
+    temp = random.randint(0, 1)
+    print(temp)
+    if temp == 0:
+        counter += 1
+if counter <= N // 2:
+    print(f"Нужно перевернуть {counter} монет")
+else:
+    print(f"Нужно перевернуть {N - counter} монет")
