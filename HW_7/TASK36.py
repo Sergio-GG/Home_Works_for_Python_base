4d2704b5279cde23eccde111d555d2762d751555
Sergio <sergio-guitar@yandex.ru>
2023-05-24 11:04:54 +0300

    "36 task hw_7 completed"

---
+++ HW_7/TASK36.py
@@ -0,0 +1,12 @@
+# Задача 36. Вывести таблицу с помощью функции высшего порядка.
+
+
+def print_operation_table(operator, num_rows, num_columns):
+    for i in range(1, num_columns + 1):
+        print(i, end="\t")
+        for j in range(2, num_rows + 1):
+            print(operator(i, j), end="\t")
+        print(end="\n")
+
+
+print_operation_table(lambda x, y: x * y, num_rows=6, num_columns=6)
